a0bac915f5f2975cda708b639389dc1791dd17c5
Jumpei Ogawa <phanective@gmail.com>
2022-11-19 19:31:40 +0900

    Add reproduction

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,2 +1,21 @@
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/i18n-edge'],
+
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        iso: 'en',
+        name: 'English',
+      },
+      {
+        code: 'ja',
+        iso: 'ja-JP',
+        name: '日本語',
+      },
+    ],
+    vueI18n: {
+      legacy: false,
+    },
+  },
 });
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    {{ t("message") }}
+    <h2>Languages</h2>
+    <div>
+      <a
+        v-for="locale in locales"
+        :key="locale.code"
+        :href="switchLocalePath(locale.code)"
+      >
+        {{ locale.name }}
+      </a>
+    </div>
+  </div>
+</template>
+
+<i18n>
+{
+  en: {
+    message: "English message"
+  },
+  ja: {
+    message: "Japanese message"
+  }
+}
+</i18n>
+
+<script setup>
+const { locales, t } = useI18n();
+// console.log(locales.value) // Cannot read properties of undefined (reading 'value')
+const switchLocalePath = useSwitchLocalePath();
+</script>
